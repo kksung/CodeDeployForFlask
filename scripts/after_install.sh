310c1a90ab611d04793769723628ee47d31d19f3
KISUNG KIM <110016279+kksung@users.noreply.github.com>
2023-11-17 10:25:38 +0900

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -18,3 +18,6 @@ rm -rf appspec.yml requirements.txt
 
 echo ">>> change owner to ubuntu -----------"
 chown -R ubuntu /home/ubuntu/hello-flask
+
+echo ">>> run app --------------------------"
+python3 -u app.py > /dev/null 2> /dev/null < /dev/null &
